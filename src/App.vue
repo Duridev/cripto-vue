7902bd10d3ccd9337f5c555db18c0f6b447b1a62
duridev <duridev@outlook.com>
2025-01-07 01:13:38 -0300

    Comenzando la aplicacion

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <script setup>
+import { ref } from 'vue'
+
+
+const monedas = ref([
+  { codigo: 'USD', texto: 'Dolar de Estados Unidos' },
+  { codigo: 'CLP', texto: 'Peso Chileno' },
+  { codigo: 'MXN', texto: 'Peso Mexicano' },
+  { codigo: 'EUR', texto: 'Euro' },
+])
 
 
 </script>
@@ -6,6 +15,25 @@
 
 <template>
 
-    
-</template>
+  <div class="contenedor">
+    <h1 class="titulo">Cotizadoer de <span>Criptomonedas</span></h1>
 
+    <div class="contenido">
+      <form class="formulario">
+        <div class="campo">
+          <label for="moneda">Moneda:</label>
+          <select id="moneda">
+            <option value="">-- Selecciona --</option>
+            <option 
+              v-for="moneda in monedas" 
+              :value="moneda.codigo"
+              >{{ moneda.texto }}
+            </option>
+          </select>
+        </div>
+      </form>
+    </div>
+
+  </div>
+
+</template>
